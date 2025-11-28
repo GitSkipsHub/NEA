d87c29f874f189855412141a1203270a7694eaaa
GitSkipsHub <kaushalthatta@gmail.com>
2025-11-28 10:59:05 +0000

    Lesson 10

--- bff/database.py
+++ bff/database.py
@@ -1,10 +1,14 @@
+from platformdirs import user_log_dir
 from pymongo import mongo_client, MongoClient
+from typing import Dict, List, Optional
+from datetime import datetime
 
 db_uri = "mongodb://localhost:27017/"
 
 db_name = "smart_skipper_db"
 
 class Database:
+
     instance = None
     client = None
     db = None
@@ -28,10 +32,49 @@ class Database:
             print(f"Failed to connect to MongoDB {e}")
             raise
 
+    def get_collection(self, collection_name: str):
+        return self.db[collection_name]
+
     def close(self):
         if self.client:
             self.client.close()
             print("Database connection closed")
+
+
+class AccountDB:
+
+    def __init__(self):
+        self.db = Database()
+        self.collection = self.db.get_collection("account")
+
+    def create_account(self, username: str, hashed_password: str) -> bool:
+        try:
+            account = {
+                "username": username,
+                "hashed_password": hashed_password,
+                "created_date": datetime.now()
+            }
+            self.collection.insert_one(account)
+            return True
+        except:
+            return False
+
+    def find_account(self, username: str) -> bool:
+        return self.collection.find_one({"username": username})
+
+    def username_exists(self, username: str) -> bool:
+        return self.collection.find_one({"username": username})
+
+class PlayerDB:
+
+    def __init__(self):
+        self.db = Database()
+        self.collection = self.db.get_collection("player")
+
+
+
+
+
 
 
 
--- gui/auth.py
+++ gui/auth.py
@@ -1,5 +1,13 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
-#from models import Account --> Need to make models.py & Account Class
+from tkinter import messagebox, ttk
+from bff.classModels import Account
+from bff.database import AccountDB
+
+class StartWindow:
+
+    def __init__(self, root):
+        self.root = root
+        self. root.title("SMART SKIPPER")
+
 
 
