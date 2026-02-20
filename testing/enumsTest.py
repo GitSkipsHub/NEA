279644be2598a5062347ccfd20c9f7c2e2168964
GitSkipsHub <kaushalthatta@gmail.com>
2026-02-20 12:14:50 +0000

    Tweaks

--- testing/enumsTest.py
+++ testing/enumsTest.py
@@ -1,53 +1,20 @@
-from bff.enums import (BowlingStyle, BattingStyle, PlayerRole, TimePeriod, HowOut, MatchType, MatchFormat, Venue, TossResult, Result)
+from bff.enums import (BowlingStyle, BattingStyle, PlayerRole)
 
-
-#BASIC ENUMS TEST
+#ENUM METHODS TEST
 
 #TEST 1
-role = PlayerRole.WKT_KEEPER
-print(role)
-print(role.value)
-print(role.name)
+print(BowlingStyle.get_value("LAOS"))
 
 #TEST 2
-role_from_key = PlayerRole["BOWLER"]
-print(role_from_key)
+print(BattingStyle.get_key("LEFT HAND"))
 
 #TEST 3
-try:
-    print(PlayerRole["bowler"])
-except KeyError:
-    print("INVALID KEY --> bowler IS NOT AN ENUM KEY")
+print(PlayerRole.list_values())
 
 #TEST 4
-try:
-    invalid_role = PlayerRole["INVALID"]
-except KeyError:
-    print("INVALID KEY --> INVALID IS NOT AN ENUM KEY")
+print(PlayerRole.get_key("bowler"))
 
 #TEST 5
-for prole in PlayerRole:
-    print(f"{prole.name} == {prole.value}")
+print(PlayerRole.get_value("INVALID_ROLE"))
 
 
-
-
-# print(BowlingStyle.get_value("LAOS"))
-#
-# print(BattingStyle.get_key("LEFT HAND"))
-#
-# print(PlayerRole.list_values())
-#
-# print(TimePeriod.get_value("L12M"))
-#
-# print(HowOut.get_key("FIELD OBSTRUCTION"))
-#
-# print(MatchType.list_values())
-#
-# print(MatchFormat.get_value("FORTY_OVERS"))
-#
-# print(Venue.get_key("HOME"))
-#
-# print(Result.list_values())
-#
-# print(TossResult.get_value("WON_BAT"))
