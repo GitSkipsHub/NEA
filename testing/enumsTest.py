3c75e7b9bca613966bf65d3639b033a9ab2fdd44
GitSkipsHub <kaushalthatta@gmail.com>
2026-02-20 12:05:02 +0000

    Tweaks

--- testing/enumsTest.py
+++ testing/enumsTest.py
@@ -25,30 +25,29 @@ try:
 except KeyError:
     print("INVALID KEY --> INVALID IS NOT AN ENUM KEY")
 
-
-
 #TEST 5
 for prole in PlayerRole:
     print(f"{prole.name} == {prole.value}")
 
 
 
-print(BowlingStyle.get_value("LAOS"))
 
-print(BattingStyle.get_key("LEFT HAND"))
-
-print(PlayerRole.list_values())
-
-print(TimePeriod.get_value("L12M"))
-
-print(HowOut.get_key("FIELD OBSTRUCTION"))
-
-print(MatchType.list_values())
-
-print(MatchFormat.get_value("FORTY_OVERS"))
-
-print(Venue.get_key("HOME"))
-
-print(Result.list_values())
-
-print(TossResult.get_value("WON_BAT"))
+# print(BowlingStyle.get_value("LAOS"))
+#
+# print(BattingStyle.get_key("LEFT HAND"))
+#
+# print(PlayerRole.list_values())
+#
+# print(TimePeriod.get_value("L12M"))
+#
+# print(HowOut.get_key("FIELD OBSTRUCTION"))
+#
+# print(MatchType.list_values())
+#
+# print(MatchFormat.get_value("FORTY_OVERS"))
+#
+# print(Venue.get_key("HOME"))
+#
+# print(Result.list_values())
+#
+# print(TossResult.get_value("WON_BAT"))
